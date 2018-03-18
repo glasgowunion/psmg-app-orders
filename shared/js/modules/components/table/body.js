203f048fb573f5b294a40f9daffb14e07bbffa05
James Laux <jamie@bleeppurple.com>
2018-03-18 22:04:38 +0000

    move table component to a single file

--- shared/js/modules/components/table/body.js
+++
@@ -1,9 +0,0 @@
-function TableBody(rows) {
-	return {
-		id: 'orderData',
-		$type: 'tbody',
-		$components: rows
-	};
-}
-
-export { TableBody };
--- shared/js/modules/components/table/table.js
+++ shared/js/modules/components/table/table.js
@@ -1,3 +1,18 @@
+function LayoutTable(table) {
+	return {
+		$type: 'div',
+		$cell: true,
+		class: 'columns',
+		$components: [
+			{
+				$type: 'div',
+				class: 'column is-12',
+				$components: [table]
+			}
+		]
+	};
+}
+
 function Table(header, body) {
 	return {
 		id: 'component-orders-table',
@@ -7,4 +22,58 @@ function Table(header, body) {
 	};
 }
 
-export { Table };
+// Header : Create a HTML table header for the orders
+// @params {[string]} - Array of header titles
+function TableHeader(headers) {
+	return {
+		$type: 'thead',
+		$components: [{ $type: 'tr', $components: headers.map(mapFunc) }]
+	};
+}
+
+function mapFunc(item) {
+	return { $type: 'th', $text: item };
+}
+
+function TableBody(rows) {
+	return {
+		id: 'orderData',
+		$type: 'tbody',
+		$components: rows
+	};
+}
+
+function TableRow(id, data, components) {
+	return {
+		$type: 'tr',
+		_data: item,
+		id: item._id,
+		$components: components
+	};
+}
+
+function TableCell(components) {
+	return {
+		$type: 'td',
+		$components: components
+	};
+}
+
+function TableCellText(text) {
+	return { $type: 'td', $text: text };
+}
+
+function TableCellTextWithComponent(text, component) {
+	return TableCell([{ $type: 'span', $text: text }, component]);
+}
+
+export {
+	LayoutTable,
+	Table,
+	TableHeader,
+	TableBody,
+	TableRow,
+	TableCell,
+	TableCellText,
+	TableCellTextWithComponent
+};
